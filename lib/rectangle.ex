44b2196d3298d1d7b01ea7c31d59a125a74651c5
Andres Rodriguez <darkknight64@gmail.com>
2017-07-26 16:43:39 -0500

    correction module rectangle

--- lib/rectangle.ex
+++ lib/rectangle.ex
@@ -1,11 +1,11 @@
-defmodule Rectangle do
+defmodule Rectangle do #Define a module
   def area_loop do #Define a function area_loop
      receive do # Listen a petition
       {:area, w, h} ->  #Define a patron "area"
-        IO.puts{"Area = #{w*h}"}
+        IO.puts("Area = #{w*h}") # Print the area
         area_loop() #Recall the function
       {:pmeter, w, h} ->
-        IO.puts{"pmter = #{w+h}"}
+        IO.puts("pmter = #{w+h}")
         area_loop()
     end
  end
